9d6a3cc925cf584a9652485b53b40202bebe806b
PijusSerapinasMipsas <pijus.s@mipsas.com>
2022-01-28 23:21:48 +0200

    Added og:image

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ const Home: NextPage = () => {
       <Head>
         <title>3speak wallet</title>
         <meta name="description" content="3Speak wallet web app" />
+        <meta name="og:image" content="https://3speak.tv/img/3S_logo.svg" />
         <link rel="icon" href="https://3speak.tv/favicon.png" />
       </Head>
       <Market />
