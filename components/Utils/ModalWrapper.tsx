8255a8d4639654fb7acf7151827fb61c30003563
PijusSerapinasMipsas <pijus.s@mipsas.com>
2022-01-30 15:22:21 +0200

    Added modal wrapper

---
+++ components/Utils/ModalWrapper.tsx
@@ -0,0 +1,27 @@
+import React, { MouseEventHandler } from "react";
+import { ImCross } from "react-icons/im";
+
+interface IModalWrapper {
+  handleClose: any;
+}
+
+export const ModalWrapper: React.FC<IModalWrapper> = ({
+  handleClose,
+  children,
+}) => {
+  return (
+    <div className="fixed top-0 left-0 flex justify-center items-center h-screen w-screen bg-gray-700 bg-opacity-50 z-50">
+      <div className="p-8 bg-gray-700 rounded-xl border-4 border-gray-800 relative">
+        <button className="m-2 absolute top-0 right-0">
+          <ImCross
+            size={15}
+            color="#fff"
+            opacity={100}
+            onClick={handleClose as MouseEventHandler}
+          />
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
